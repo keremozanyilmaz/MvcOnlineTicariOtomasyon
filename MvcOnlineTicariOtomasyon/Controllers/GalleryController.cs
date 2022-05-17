4937efb0fdde4c98ae2e59382c7c488bdde78e92
Kerem Ozan Yılmaz <keremozanyilmaz@gmail.com>
2022-05-17 22:58:24 +0300

    Gallery added

---
+++ MvcOnlineTicariOtomasyon/Controllers/GalleryController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Classes;
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class GalleryController : Controller
+    {
+        // GET: Gallery
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var values = c.Products.ToList();
+            return View(values);
+        }
+    }
+}
